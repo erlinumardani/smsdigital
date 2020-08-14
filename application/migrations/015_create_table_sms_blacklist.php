69519fbdcd99d6785aa9088f00d274f74046d168
Erlin <erlinumardani@gmail.com>
2020-08-14 08:21:38 +0700

    blacklist

---
+++ application/migrations/015_create_table_sms_blacklist.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @author   Natan Felles <natanfelles@gmail.com>
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Class Migration_create_table_users
+ *
+ * @property CI_DB_forge         $dbforge
+ * @property CI_DB_query_builder $db
+ */
+class Migration_create_table_sms_blacklist extends CI_Migration {
+
+
+	protected $table = 'sms_blacklist';
+
+
+	public function up()
+	{
+		$fields = array(
+			'id' => [
+				'type' => 'INT(11)',
+				'auto_increment' => TRUE
+			],
+			'phone'  => [
+				'type' => 'VARCHAR(50)'
+			],
+			'updated_by' => [
+				'type' => 'VARCHAR(20)',
+				'null' => TRUE,
+				'unsigned' => TRUE,
+				'default' => 'System'
+			],
+			'created_at' => [
+				'type' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP' 
+			],
+			'updated_at' => [
+				'type' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+				'null' => TRUE
+			]
+		);
+		$this->dbforge->add_field($fields);
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table($this->table, TRUE);
+
+	}
+
+
+	public function down()
+	{
+		if ($this->db->table_exists($this->table))
+		{
+			$this->dbforge->drop_table($this->table);
+		}
+	}
+
+}
