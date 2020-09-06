a40c23191ab6c60ddd24a4371fc8fe0e6c432de6
Erlin Umar Dani <erlin@MacAir.local>
2020-09-06 21:56:30 +0700

    number fix

--- application/views/dashboard/js.php
+++ application/views/dashboard/js.php
@@ -187,7 +187,7 @@ $(document).ready(function() {
             $('#total_sms_received').text(data.total_sms_received);
             $('#total_sms_sending').text(data.total_sms_sending);
             $('#total_sms_failed').text(data.total_sms_failed);
-            $('#total_sms').text(parseInt(data.total_sms_received) + parseInt(data.total_sms_sending));
+            $('#total_sms').text(parseInt(data.total_sms_received.replace(/,/g, "")) + parseInt(data.total_sms_sending.replace(/,/g, "")));
             $('#summary').unblock();
         }
     });
